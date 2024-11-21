9dc269e14d4c7c6e4b51ce104c7d16f7aad79858
Kashyap surani <152683782+Kashyapsurani@users.noreply.github.com>
2024-11-21 14:26:11 +0530

    Add files via upload

---
+++ Ref , V-model/src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0); 
+const inputValue = ref(''); 
+
+const increment = () => {
+  count.value++; 
+};
+
+const decrement = () => {
+  count.value--; 
+};
+
+const reset = () => {
+  count.value = 0; 
+};
+
+const setCount = () => {
+  count.value = parseInt(inputValue.value) || 0;
+};
+</script>
+
+<template>
+  <h1>Counter: {{ count }}</h1>
+  <button @click="increment">Increment</button>
+  <button @click="decrement">Decrement</button>
+  <button @click="reset">Reset</button>
+  <input type="text" placeholder="Enter a value" v-model="inputValue">
+  <button @click="setCount">Set Count</button>
+
+</template>
---
+++ V-model/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue'
+
+// Create reactive references
+const name = ref('');
+const email = ref('');
+
+const submitForm = () => {
+  alert(`Name: ${name.value}\nEmail: ${email.value}`);
+};
+</script>
+
+<template>
+  <h1>Simple Form Example</h1>
+  <label for="name">Name:</label>
+  <input type="text" id="name" v-model="name" placeholder="Enter your name">
+
+  <label for="email">Email:</label>
+  <input type="email" id="email" v-model="email" placeholder="Enter your email">
+
+  <p>Your name is: {{ name }}</p>
+  <p>Your email is: {{ email }}</p>
+
+  <button @click="submitForm">Submit</button>
+
+</template>
